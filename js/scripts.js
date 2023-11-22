1d62e5e53980db6d4960245b027c6beea0d2c28a
adeniran adejuwon <adeniran adejuwon>
2023-11-22 02:00:21 -0800

    add function 1

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function() {
+
+
+function convertTbsptoCup(number1){
+  let number2 =3 / 8 * number1
+  return number2;
+}
+
+function convertTbsptoCup(number2){
+  let number1 =4 / 16 *number2
+  return number1;
+}
+function convertTbsptoCup(number1){
+  let number2 =5 / 20 *number1
+  return number2;
+}
+function convertTbsptoCup(number2){
+  let number1 =6/ 24*number2
+  return number1;
+}
+function convertTbsptoCup(number1){
+  let number2 =3/ 21*number1
+  return number2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
